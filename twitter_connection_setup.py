97cd797ce5131277bcf5026893c260b9b1f0e4ec
iflot <106816609+iflot@users.noreply.github.com>
2022-06-05 12:05:58 +0200

    Create twitter_connection_setup.py

---
+++ twitter_connection_setup.py
@@ -0,0 +1,18 @@
+import tweepy
+# Import des clés d'accès API Twitter
+from credentials import *
+
+def twitter_setup():
+    """
+    Utility function to setup the Twitter's API
+    with an access keys provided in a file credentials.py
+    :return: the authentified API
+    """
+    # Authentification : 
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+    # Retour API :
+    api = tweepy.API(auth)
+    return api
+twitter_setup()
