bc3c2636ecec2468d189ad570710f6ed398d53ae
iflot <106816609+iflot@users.noreply.github.com>
2022-06-05 12:06:37 +0200

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,11 @@
+"""
+    Identifiants twitter_developper 
+"""
+# Consume:
+CONSUMER_KEY = " "
+CONSUMER_SECRET = " "
+
+# Access:
+# Find those as an app access credentials (you have to create an app on the website)
+ACCESS_TOKEN = " "
+ACCESS_SECRET = " "
