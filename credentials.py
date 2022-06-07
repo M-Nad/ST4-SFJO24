052c309a871a7cc5b119459df5fe2c3873cd466b
iflot <106816609+iflot@users.noreply.github.com>
2022-06-07 15:18:14 +0200

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -2,10 +2,10 @@
     Identifiants twitter_developper 
 """
 # Consume:
-CONSUMER_KEY = "Q7TAEyI6Pv4HLLFJXWmSGBPSX"
-CONSUMER_SECRET = "FRBlep29RgAJcqJ8DvUhwZGkjroOHRJqFhSPtotFLhNKPbtzve"
+CONSUMER_KEY = "HEunqpj36iOvHn1fvTiiEAp11"
+CONSUMER_SECRET = "GR2zrbU7swfllPQKgusaRprdk8R3deWjt90RGlrqyaPb4r06ee"
 
 # Access:
 # Find those as an app access credentials (you have to create an app on the website)
-ACCESS_TOKEN = "1456391594144419845-lmDorQlH8Gbupo1W9cYpjNnfYv8Qwv"
-ACCESS_SECRET = "FecH9ktDrIDNnRhkZVguXgVsrroRQoc2MVCFPgWq5UILH"
+ACCESS_TOKEN = "1456391594144419845-wWIPVgmHqf6I8pd2NXZKDgyQ2cw1CX"
+ACCESS_SECRET = "cGXLlYiji9cBdDfDfw7i2p3Iuy7gzrKje8gbXMrgLLKMI"
