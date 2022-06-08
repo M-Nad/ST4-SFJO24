1811cfa6a3ab753a03f9f514198b3bad67730d6b
iflot <106816609+iflot@users.noreply.github.com>
2022-06-08 14:38:21 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import twitter_connection_setup
 
 api = twitter_connection_setup.twitter_setup()
@@ -5,18 +6,23 @@ api = twitter_connection_setup.twitter_setup()
 def scrapping(query):
 
     #Nombre de tweets récupérés
-    number_of_tweets = 1
+    number_of_tweets = 50
 
     #Récupération des tweets
-    tweets = api.search_tweets(q = query, lang = "fr", count = number_of_tweets, tweet_mode="extended")
+    tweets = api.search_tweets(q = query + + " " + "since_time:1654345488 until_time:1654359888" + " " + "-filter:retweets AND -filter:links", lang = "en", count = number_of_tweets, tweet_mode="extended")
     liste_tweet = []
     for tweet in tweets:
         info = {}
         info["id"] = tweet._json["id"]
         date = '2022-06-' + tweet._json["created_at"][8:10] + ' ' + tweet._json["created_at"][11:19]
         info["date"] = date
-        info["text"] = tweet._json["full_text"] 
+        texte = tweet._json["full_text"]
+        #texte.replace(" ' ", ' " ')
+        texte = texte.strip('\n')
+        info["text"] = texte
+        info["nombre_likes"] = tweet._json["favorite_count"]
+        info["nombre_retweets"] = tweet._json["retweet_count"]
         liste_tweet.append(info)
         print(info)
     return liste_tweet
-scrapping("test")
+scrapping("boris johnson")
