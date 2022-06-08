955ab95861552dfe0067b81f7d33cf7542a210dd
NADALIN Marius <marius.nadalin@student-cs.fr>
2022-06-08 23:38:48 +0200

    save classifier after training

--- tree_classifier.py
+++ tree_classifier.py
@@ -4,43 +4,54 @@ from sklearn import tree
 from nltk import word_tokenize          
 from nltk.stem import WordNetLemmatizer 
 import json
+import pickle
 import pandas as pd
 import utils.lemma_token as LT
 
 csv_path = '.\csv\database08.csv'
 
-tweet_tokenizer = LT.tweet_tokenizer_lemmatizer
+def regenerate_classifier(csv_path = '.\csv\database08.csv',filename = 'tree_classifier.sav'):
 
-#univers du vocabulaire pour la vectorisation
-with open('vocabulary_dic.json') as f:
-    voc = json.load(f)
+    tweet_tokenizer = LT.tweet_tokenizer_lemmatizer
 
-# données d'apprentissage / labels
+    #univers du vocabulaire pour la vectorisation
+    with open('vocabulary_dic.json') as f:
+        voc = json.load(f)
 
-"""
-X_data = ["positive","null","positive","negative"]
-Y_label = [1,0,1,-1]
-"""
-Data = pd.read_csv(csv_path)
-X_data = Data["text"].to_list()
-Y_label = Data["sentiment"].to_list()
+    # données d'apprentissage / labels
+    Data = pd.read_csv(csv_path)
+    X_data = Data["text"].to_list()
+    Y_label = Data["sentiment"].to_list()
+
+    # traitement / vectorisation des entrées
+    count_vect = CountVectorizer(vocabulary=voc,tokenizer=tweet_tokenizer)
+    X_train_counts = count_vect.fit_transform(X_data)
+
+    tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
+    X_train_tf = tf_transformer.transform(X_train_counts)
+
+    # entrainement du classifier
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(X_train_tf, Y_label)
+
+    pickle.dump(clf, open(filename, 'wb'))
 
 
-# traitement / vectorisation des entrées
-count_vect = CountVectorizer(vocabulary=voc,tokenizer=tweet_tokenizer)
-X_train_counts = count_vect.fit_transform(X_data)
+def predict(X_data,csv_path = '.\csv\database08.csv',filename = 'tree_classifier.sav'):
 
-tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
-X_train_tf = tf_transformer.transform(X_train_counts)
+    with open('vocabulary_dic.json') as f:
+        voc = json.load(f)
+    tweet_tokenizer = LT.tweet_tokenizer_lemmatizer
+    Data = pd.read_csv(csv_path)
+    X_train_data = Data["text"].to_list()
+    count_vect = CountVectorizer(vocabulary=voc,tokenizer=tweet_tokenizer)
+    X_train_counts = count_vect.fit_transform(X_train_data)
+    tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
 
-# entrainement du classifier
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X_train_tf, Y_label)
-
-def predict(X_data):
+    loaded_clf = pickle.load(open(filename, 'rb'))
     X_counts = count_vect.fit_transform(X_data)
     X = tf_transformer.transform(X_counts)
-    Y_predicted = clf.predict(X)
+    Y_predicted = loaded_clf.predict(X)
     return Y_predicted.tolist()
 
 
