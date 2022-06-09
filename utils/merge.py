b920dba511c583a5a89adb5d52c38383a04cb36e
de_Laboulaye Bertrand <bertrand.de-laboulaye@student-cs.fr>
2022-06-09 11:43:43 +0200

    database apprentissage

--- utils/merge.py
+++ utils/merge.py
@@ -2,13 +2,13 @@ import pandas as pd
 
 # Names for database
 
-names = ["database.csv",
-"database2.csv","database2.csv"]
+names = ["C:/Users/bertd/OneDrive/Bureau/Scolaire 1A CS/EI ST4/ST4-SFJO24/csv/database.csv",
+         "C:/Users/bertd/OneDrive/Bureau/Scolaire 1A CS/EI ST4/ST4-SFJO24/csv/database2.csv", "C:/Users/bertd/OneDrive/Bureau/Scolaire 1A CS/EI ST4/ST4-SFJO24/csv/database08.csv", "C:/Users/bertd/OneDrive/Bureau/Scolaire 1A CS/EI ST4/ST4-SFJO24/csv/databasebis05.csv", "C:/Users/bertd/OneDrive/Bureau/Scolaire 1A CS/EI ST4/ST4-SFJO24/csv/databasebisn.csv", "C:/Users/bertd/OneDrive/Bureau/Scolaire 1A CS/EI ST4/ST4-SFJO24/csv/databaset2.csv", "C:/Users/bertd/OneDrive/Bureau/Scolaire 1A CS/EI ST4/ST4-SFJO24/csv/databaset3.csv", "C:/Users/bertd/OneDrive/Bureau/Scolaire 1A CS/EI ST4/ST4-SFJO24/csv/databaset5.csv"]
 
 # read them all
 dataframes = []
 for elem in names:
     dataframes.append(pd.read_csv(elem))
-res = pd.concat(dataframes,ignore_index=True)
+res = pd.concat(dataframes, ignore_index=True)
 
-res.to_csv("db.csv")
+res.to_csv("db_apprentissage.csv")
