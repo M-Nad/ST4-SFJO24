cd3fb0b42d3069dd15fbab4e819aec4ec4796ba9
NADALIN Marius <marius.nadalin@student-cs.fr>
2022-06-09 11:20:57 +0200

    sql create update

--- SQL_DB/sql_create.py
+++ SQL_DB/sql_create.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect("./apprentissage.db")
+conn = sqlite3.connect("./tweet_SQL_database.db")
 
 print("Opened database successfully");
 
@@ -10,7 +10,7 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS TWEET (
   tweet_id INT PRIMARY KEY,
   user_id VARCHAR(40) NOT NULL,
-  date TEXT,
+  date DATETIME,
   text TEXT, 
   CONSTRAINT U_userID_date UNIQUE (user_id,date)
 );
@@ -20,6 +20,13 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS SCORE (
   tweet_id INT PRIMARY KEY,
   score INT NOT NULL
+);
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS SCORE_SENTIWORDNET (
+  tweet_id INT PRIMARY KEY,
+  score_sentiwordnet INT NOT NULL
 );
 """)
 
